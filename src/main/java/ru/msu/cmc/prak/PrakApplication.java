188ad85591e4f8c76cc47910b6615e54eb6b5fd0
DjBROV <vacheslav.bogatenkov@gmail.com>
2026-03-15 18:58:28 +0300

    init

---
+++ src/main/java/ru/msu/cmc/prak/PrakApplication.java
@@ -0,0 +1,13 @@
+package ru.msu.cmc.prak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrakApplication.class, args);
+	}
+
+}
